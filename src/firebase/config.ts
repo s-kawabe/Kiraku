66555e470aae76d2224b01517ca17fec83dda470
shintarou kawabe <s.kawabe2281@gmail.com>
2021-03-20 16:07:32 +0900

    :triangular_flag_on_post: add firebase config
    add firebase config

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBsQ8j3hQ2t7QMlnBbPXDrbZH308uaTtCs',
+  authDomain: 'kiraku-7ea19.firebaseapp.com',
+  projectId: 'kiraku-7ea19',
+  storageBucket: 'kiraku-7ea19.appspot.com',
+  messagingSenderId: '936906563555',
+  appId: '1:936906563555:web:4c1aeba0fc95fb188e4ad6',
+  measurementId: 'G-M4X35MZW77',
+}
