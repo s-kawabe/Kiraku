bb58c8b31a6c3922d50bfd031bbf8619412f7853
shintarou kawabe <s.kawabe2281@gmail.com>
2021-05-08 14:10:39 +0900

    :bug: fix util files import extension

--- src/components/blog/container/BlogForms.tsx
+++ src/components/blog/container/BlogForms.tsx
@@ -21,9 +21,9 @@ import { useState } from 'react'
 import { BlogEditor } from '@/components/blog/unit'
 import { GenderRadioButton } from '@/components/common/unit'
 import type { Gender } from '@/utils/constants/Common'
-import { insertBlogToHasura } from '@/utils/methods/blog'
-import { useAllTopicsAndBrands } from '@/utils/methods/customeHooks'
-import { addTagAttribute, checkExistTable } from '@/utils/methods/post'
+import { insertBlogToHasura } from '@/utils/methods/blog.ts'
+import { useAllTopicsAndBrands } from '@/utils/methods/customeHooks.tsx'
+import { addTagAttribute, checkExistTable } from '@/utils/methods/post.ts'
 // 編集の時はblog１件分のデータがpropsに入ってくる
 // type Props = {
 //   blogData?: any
