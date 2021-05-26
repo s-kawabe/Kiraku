09d4e6fc1c5e4d95f3923a0f1c56ef49c6cfd0bc
shintarou kawabe <s.kawabe2281@gmail.com>
2021-05-27 03:15:49 +0900

    :wrench: update gql

--- src/pages/brands/[brandId]/index.tsx
+++ src/pages/brands/[brandId]/index.tsx
@@ -1,3 +1,4 @@
+import { gql } from '@apollo/client'
 import { useRouter } from 'next/router'
 
 import { LayoutWithHead } from '@/components/layout/container'
@@ -16,3 +17,63 @@ const BrandPage = () => {
 
 // eslint-disable-next-line import/no-default-export
 export default BrandPage
+
+gql`
+  query GetAllBrands {
+    brands {
+      id
+      name
+    }
+  }
+`
+
+gql`
+  query GetBrandWithPostAndBlog($brandId: Int!, $limit: Int!, $offset: Int!) {
+    posts(where: { brands: { brand_id: { _eq: $brandId } } }, limit: $limit, offset: $offset) {
+      id
+      image
+      gender
+      content
+      created_at
+      user {
+        id
+        display_id
+        image
+        name
+      }
+      comments_aggregate {
+        aggregate {
+          count(columns: id)
+        }
+      }
+      likes_aggregate {
+        aggregate {
+          count(columns: id)
+        }
+      }
+    }
+    blogs(where: { brands: { brand_id: { _eq: $brandId } } }, limit: $limit, offset: $offset) {
+      id
+      title
+      content
+      gender
+      created_at
+      user {
+        id
+        display_id
+        image
+        name
+      }
+      comments_aggregate {
+        aggregate {
+          count(columns: id)
+        }
+      }
+      likes_aggregate {
+        aggregate {
+          count(columns: id)
+        }
+      }
+    }
+  }
+`
--- src/pages/topics/[topicId]/index.tsx
+++ src/pages/topics/[topicId]/index.tsx
@@ -1,3 +1,4 @@
+import { gql } from '@apollo/client'
 import { useRouter } from 'next/router'
 
 import { LayoutWithHead } from '@/components/layout/container'
@@ -16,3 +17,63 @@ const TopicPage = () => {
 
 // eslint-disable-next-line import/no-default-export
 export default TopicPage
+
+gql`
+  query GetAllTopics {
+    topics {
+      id
+      name
+    }
+  }
+`
+
+gql`
+  query GetTopicWithPostAndBlog($topicId: Int!, $limit: Int!, $offset: Int!) {
+    posts(where: { topics: { topic_id: { _eq: $topicId } } }, limit: $limit, offset: $offset) {
+      id
+      image
+      gender
+      content
+      created_at
+      user {
+        id
+        display_id
+        image
+        name
+      }
+      comments_aggregate {
+        aggregate {
+          count(columns: id)
+        }
+      }
+      likes_aggregate {
+        aggregate {
+          count(columns: id)
+        }
+      }
+    }
+    blogs(where: { topics: { topic_id: { _eq: $topicId } } }, limit: $limit, offset: $offset) {
+      id
+      title
+      content
+      gender
+      created_at
+      user {
+        id
+        display_id
+        image
+        name
+      }
+      comments_aggregate {
+        aggregate {
+          count(columns: id)
+        }
+      }
+      likes_aggregate {
+        aggregate {
+          count(columns: id)
+        }
+      }
+    }
+  }
+`
